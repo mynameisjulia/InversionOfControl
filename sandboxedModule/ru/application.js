841ee874dc9df4624405922283f5ed36901b3143
Julia Shurygina <gylianna01@gmail.com>
2016-03-18 13:01:15 +0200

    task 1-5

--- sandboxedModule/ru/application.js
+++ sandboxedModule/ru/application.js
@@ -5,16 +5,18 @@
 // Вывод из глобального контекста модуля
 console.log('From application global context');
 
+console.log(util.isArray([]));
+console.log('Hello from the other side');
+
+setTimeout(function () {
+  console.log('2000ms timeout');
+}, 2000);
+
+setInterval(function () {
+  console.log('1000ms passed');
+}, 1000);
+
 module.exports = function() {
   // Вывод из контекста экспортируемой функции
   console.log('From application exported function');
 };
-
-setTimeout(function () {
-  console.log("2000ms timeout");
-},500);
-
-function sec() {
-  console.log("sec passed");
-}
-setInterval(sec,500);
--- sandboxedModule/ru/framework.js
+++ sandboxedModule/ru/framework.js
@@ -5,22 +5,59 @@
 
 // Фреймворк может явно зависеть от библиотек через dependency lookup
 var fs = require('fs'),
-    vm = require('vm');
+    vm = require('vm'),
+    util = require('util');
+
+var fileName = './application.js';
+if (process.argv[2] !== undefined) {
+  fileName = process.argv[2];
+}
+var logfile = 'awesome.log';
+
+function writeToFile(message) {
+  fs.appendFile(logfile, `${message}\n`, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
+}
+
+function createFile() {
+  fs.writeFile(logfile, '', (err) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
+}
 
 // Создаем контекст-песочницу, которая станет глобальным контекстом приложения
-var context = { module: {}, console: console, setTimeout:setTimeout,setInterval:setInterval  };
+var context = {
+  module: {},
+  console: {
+    log: (message) => {
+      const date = new Date();
+      const text = `${fileName} ${date.toUTCString()} ${message}`;
+      console.log(text);
+      writeToFile(text);
+    }
+  },
+setTimeout: setTimeout,
+    setInterval: setInterval,
+    util: util
+};
 context.global = context;
 var sandbox = vm.createContext(context);
 
-// Читаем исходный код приложения из файла
-var fileName = './application.js';
 fs.readFile(fileName, function(err, src) {
-  // Тут нужно обработать ошибки
-  
+  if (err) {
+    return console.log(err);
+  }
+
+
   // Запускаем код приложения в песочнице
   var script = vm.createScript(src, fileName);
   script.runInNewContext(sandbox);
-  
+
   // Забираем ссылку из sandbox.module.exports, можем ее исполнить,
   // сохранить в кеш, вывести на экран исходный код приложения и т.д.
 });
